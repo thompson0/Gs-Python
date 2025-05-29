3f7c37d86e87fed654b2aee3714d5ba9386f5f92
thompson0 <thompsonjooj@gmail.com>
2025-05-29 01:47:43 -0300

    trocando comandos e arrumando a logica

--- jogo.py
+++ jogo.py
@@ -1,8 +1,9 @@
+#bibliotecas
 import random
 import pygame
 
 pygame.init()
-
+# Variaveis ultilizadas 
 Mapa = [
     1, 2, 3, 4,
     5, 6, 7, 8,
@@ -24,6 +25,8 @@ fundo_img = pygame.transform.scale(fundo_img, (640, 640))
 fundo_game_over = pygame.image.load('imgs/game_over.png').convert()
 fundo_game_over = pygame.transform.scale(fundo_game_over, (640, 640))
 
+fundo_vitoria = pygame.image.load('imgs/vitoria.jpg').convert()
+fundo_vitoria = pygame.transform.scale(fundo_vitoria, (640, 640))
 pygame.mouse.set_visible(1)
 
 largura_quadrado = 80
@@ -37,7 +40,7 @@ start_x = (640 - largura_grade) // 2
 start_y = (640 - altura_grade) // 2
 fonte = pygame.font.SysFont(None, 28)
 fonte_grande = pygame.font.SysFont(None, 40)
-
+vitoria = False
 clock = pygame.time.Clock()
 running = True
 
@@ -49,19 +52,26 @@ cor_normal = (30, 60, 90)
 cor_jogador = (200, 50, 50)
 cor_texto = (255, 255, 255)
 
+clicados = []
+cor_clicado = (100,100,150)
+
+#Enquanto a janela do pygame estiver aberta
 while running:
     if tela_inicial:
         screen.blit(fundo_inicial, (0, 0))
 
+        texto2 = fonte_grande.render(msg_entrada, True, cor_texto)
+        texto2_rect = texto2.get_rect(center=(320, 320))
+        screen.blit(texto2, texto2_rect)
+        
         texto1 = fonte.render(msg_inicial, True, cor_texto)
         texto1_rect = texto1.get_rect(center=(320, 260))
         screen.blit(texto1, texto1_rect)
 
-        texto2 = fonte_grande.render(msg_entrada, True, cor_texto)
-        texto2_rect = texto2.get_rect(center=(320, 320))
-        screen.blit(texto2, texto2_rect)
     elif game_over:
         screen.blit(fundo_game_over, (0, 0))
+    elif vitoria:
+        screen.blit(fundo_vitoria,(0,0))
     else:
         screen.blit(fundo_img, (0, 0))
 
@@ -75,6 +85,8 @@ while running:
 
                 if index == posicao_atual:
                     cor = cor_jogador
+                elif index in clicados:
+                    cor = cor_clicado
                 else:
                     cor = cor_normal
 
@@ -92,18 +104,30 @@ while running:
             if event.key == pygame.K_RETURN:
                 tela_inicial = False
 
-        elif not tela_inicial and not game_over and event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN and posicao_atual + 4 < 12:
-                posicao_atual += 4
-            elif event.key == pygame.K_UP and posicao_atual - 4 >= 0:
-                posicao_atual -= 4
-            elif event.key == pygame.K_RIGHT and (posicao_atual % 4) < 3:
-                posicao_atual += 1
-            elif event.key == pygame.K_LEFT and (posicao_atual % 4) > 0:
-                posicao_atual -= 1
-            if posicao_atual + 1 == enchente:
-                print('perdeu')
-                game_over = True
+        elif not tela_inicial and not game_over and not vitoria:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Clique esquerdo
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                for l in range(3):
+                    for c in range(4):
+                        index = l * 4 + c
+
+                        x = start_x + c * (largura_quadrado + margem)
+                        y = start_y + l * (altura_quadrado + margem)
+                        rect = pygame.Rect(x, y, largura_quadrado, altura_quadrado)
+
+                        if rect.collidepoint(mouse_x, mouse_y):
+                            posicao_atual = index
+
+                            if index not in clicados:
+                                clicados.append(index)
+                                
+                            if Mapa[posicao_atual] == enchente:
+                                print('perdeu')
+                                game_over = True
+                            elif len(set(clicados)) == 11:
+                                if enchente not in clicados:     
+                                    print('Vitoria')
+                                    vitoria = True
 
     pygame.display.flip()
     clock.tick(60)
