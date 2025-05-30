c786a3602c9665dd054094f99c7160049191e975
thompson0 <thompsonjooj@gmail.com>
2025-05-30 17:40:11 -0300

    tela game over e tela inicial

--- jogo.py
+++ jogo.py
@@ -11,7 +11,7 @@ pygame.mixer.music.play(-1)
 linhas = 10
 colunas = 10
 total_casas = linhas * colunas
-total_bombas = 10
+total_bombas = 2
 
 def gerar_mapa():
     mapa = [0] * total_casas
@@ -41,7 +41,8 @@ fundo_game_over = pygame.image.load('imgs/game_over.png').convert()
 fundo_game_over = pygame.transform.scale(fundo_game_over, (800, 800))
 fundo_vitoria = pygame.image.load('imgs/vitoria.jpg').convert()
 fundo_vitoria = pygame.transform.scale(fundo_vitoria, (800, 800))
-msg_inicial = 'Chuvas intensas causaram alagamentos pela cidade. Encontre os abrigos seguros!'
+msg_inicial = 'Chuvas intensas causaram alagamentos pela cidade.'
+msg_explicao = 'Encontre os abrigos seguros!'
 msg_entrada = 'Pressione ENTER para começar'
 
 pygame.mouse.set_visible(1)
@@ -59,9 +60,9 @@ fonte_grande = pygame.font.SysFont(None, 40)
 
 # Cores
 cor_normal = (30, 60, 90)
-cor_clicado = (100, 100, 150)
-cor_texto = (20, 20, 0)
-
+cor_clicado = (211, 211, 211)
+cor_texto = (169, 169, 169)
+cor_rect_texto = (0,0,0)
 # Estados
 clicados = set()
 revelados = set()
@@ -93,24 +94,31 @@ while running:
 
     if tela_inicial:
         screen.blit(fundo_inicial, (0, 0))
-        texto1 = fonte.render(msg_inicial, True, cor_texto)
+        texto1 = fonte_grande.render(msg_inicial, True, cor_texto)
         texto1_rect = texto1.get_rect(center=(400, 200))
         screen.blit(texto1, texto1_rect)
         texto2 = fonte_grande.render(msg_entrada, True, cor_texto)
         texto2_rect = texto2.get_rect(center=(400, 660))
         screen.blit(texto2, texto2_rect)
+        texto3 = fonte_grande.render(msg_explicao, True, cor_texto)
+        texto3_rect = texto3.get_rect(center=(400, 260))
+        screen.blit(texto3, texto3_rect)
 
     elif game_over:
         screen.blit(fundo_game_over, (0, 0))
         botao_rect = pygame.Rect(300, 650, 200, 50)
-        pygame.draw.rect(screen, (0, 120, 0), botao_rect, border_radius=8)
+        pygame.draw.rect(screen, (0, 0, 0), botao_rect, border_radius=8)
         texto_botao = fonte_grande.render("Reiniciar", True, (255, 255, 255))
         texto_botao_rect = texto_botao.get_rect(center=botao_rect.center)
         screen.blit(texto_botao, texto_botao_rect)
 
     elif vitoria:
         screen.blit(fundo_vitoria, (0, 0))
-
+        botao_rect = pygame.Rect(300, 650, 200, 50)
+        pygame.draw.rect(screen, (0, 0, 255), botao_rect, border_radius=8)
+        texto_botao = fonte_grande.render("Reiniciar", True, (255, 255, 255))
+        texto_botao_rect = texto_botao.get_rect(center=botao_rect.center)
+        screen.blit(texto_botao, texto_botao_rect)
     else:
         screen.blit(fundo_inicial, (0, 0))
         for l in range(linhas):
@@ -122,7 +130,7 @@ while running:
                 if index in revelados:
                     pygame.draw.rect(screen, cor_clicado, (x, y, largura_quadrado, altura_quadrado), border_radius=5)
                     if Mapa[index] > 0:
-                        texto = fonte.render(str(Mapa[index]), True, cor_texto)
+                        texto = fonte.render(str(Mapa[index]), True, cor_rect_texto)
                         texto_rect = texto.get_rect(center=(x + largura_quadrado // 2, y + altura_quadrado // 2))
                         screen.blit(texto, texto_rect)
                 else:
@@ -148,6 +156,17 @@ while running:
                     vitoria = False
                     tela_inicial = True
                     continue
+            
+            if vitoria:
+                botao_rect = pygame.Rect(300, 650, 200, 50)
+                if botao_rect.collidepoint((mouse_x, mouse_y)):
+                    Mapa, enchentes = gerar_mapa()
+                    clicados.clear()
+                    revelados.clear()
+                    game_over = False
+                    vitoria = False
+                    tela_inicial = True
+                    continue        
 
             if not tela_inicial and not vitoria and not game_over:
                 for l in range(linhas):
